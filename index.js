dbd764ad23c78d37961d46da29f08254c360e50f
Retford <retfordjb@gmail.com>
2023-03-21 18:25:52 -0500

    Avance

---
+++ index.js
@@ -0,0 +1,17 @@
+/*Importación de clases*/
+import { Cliente } from './Cliente.js';
+import { Cuenta } from './Cuenta.js';
+import { CuentaAhorro } from './CuentaAhorro.js';
+import { CuentaCorriente } from './CuentaCorriente.js';
+import { cuentaNomina } from './CuentaNomina.js';
+
+const cliente = new Cliente('Leonardo', '13804050', '123224');
+const cliente2 = new Cliente('María', '16979808', '8989');
+
+const cuentaDeLeonardo = new CuentaCorriente(cliente, '1', '001');
+const cuentaDeMaria = new CuentaCorriente(cliente2, '2', '002');
+
+const CuentaAhorroLeonardo = new CuentaAhorro(cliente, '9985', '004', 0);
+
+const cuentaNominaLeonardo = new cuentaNomina(cliente, '9898', '003', 100);
+console.log(cuentaNominaLeonardo);
